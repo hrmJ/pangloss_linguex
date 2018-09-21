bcf5a96e45d9e22d28d47b3c6c87567949fe8b61
Juho Härme <juho.harme@gmail.com>
2018-09-21 11:29:59 +0300

    removed debug info

--- pangloss/pangloss.py
+++ pangloss/pangloss.py
@@ -23,7 +23,7 @@ def gloss_refs(elem, doc):
                 ref = "\\ref{ex:" + text[4:] + "}"
 
                 fmt = doc.get_metadata('exampleRefFormat')
-                pf.debug(fmt)
+                #pf.debug(fmt)
                 if isinstance(fmt, list):
                     ref = (fmt[0]).format(ref)
                 else:
